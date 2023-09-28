c81f1854e88ff9b38e357f35db4b7a3d1f68651d
matFS06 <131223153+matFS06@users.noreply.github.com>
2023-09-28 13:09:15 -0300

    Add files via upload

--- labirinto.py
+++ labirinto.py
@@ -1,6 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
 from collections import deque
 
 def bfs_labirinto(labirinto):
+    # Função de busca em largura (BFS) para encontrar o caminho mais curto
     def is_valid(x, y):
         return 0 <= x < len(labirinto) and 0 <= y < len(labirinto[0]) and labirinto[x][y] == ' '
 
@@ -26,52 +29,64 @@ def bfs_labirinto(labirinto):
     return None
 
 def marcar_labirinto(labirinto, caminho):
+    # Função para marcar o labirinto com X ao longo do caminho
     marked_labirinto = [list(row) for row in labirinto]
     for x, y in caminho:
         marked_labirinto[x][y] = 'X'
     return marked_labirinto
 
 def imprimir_labirinto(labirinto):
-    print("\n======== LABIRINTO ========")
+    # Função para imprimir o labirinto no console
     for row in labirinto:
         print("".join(row))
 
-def main():
-    labirinto = [
-        "#### ######################",
-        "#### ######################",
-        "##            #       #####",
-        "###### #####    ### #######",
-        "###### ##### #####     ####",
-        "###    ###    ##   ## #####",
-        "### ###### ####### ##   ###",
-        "###     ##     ###   ##  ##",
-        "### ### ######   # #  #  ##",
-        "##   ##      ##    #    ###",
-        "############### ############"
-    ]
-
+def on_ver_caminho():
     caminho = bfs_labirinto(labirinto)
 
     if caminho:
-        imprimir_labirinto(labirinto)
-        resposta = input("Quer ver o caminho mais curto? 1. SIM | 2. NÃO: ")
+        resposta = messagebox.askyesno("Caminho mais curto", "Quer ver as coordenadas do caminho mais curto?")
 
-        if resposta == '1':
-            print("\nCoordenadas do caminho mais curto:")
+        if resposta:
+            caminho_str = "\nCoordenadas do caminho mais curto:\n"
             for x, y in caminho:
-                print(f"({x}, {y})")
+                caminho_str += f"({x}, {y})\n"
 
-            print("\nCaminho mais curto:")
-            marked_labirinto = marcar_labirinto(labirinto, caminho)
-            imprimir_labirinto(marked_labirinto)
-        elif resposta == '2':
-            print("OK, obrigado!")
-        else:
-            print("Opção inválida.")
+            messagebox.showinfo("Caminho mais curto", caminho_str)
+        
+        # Atualiza o labirinto com as marcações
+        labirinto_atualizado = marcar_labirinto(labirinto, caminho)
+        labirinto_txt = "\n".join(["".join(row) for row in labirinto_atualizado])
+        labirinto_label.config(text=labirinto_txt)
     else:
-        print("Nenhum caminho encontrado.")
+        messagebox.showinfo("Caminho mais curto", "Nenhum caminho encontrado.")
 
-if __name__ == "__main__":
-    main()
-1
+labirinto = [
+    "#### ######################",
+    "#### ######################",
+    "##            #       #####",
+    "###### #####    ### #######",
+    "###### ##### #####     ####",
+    "###    ###    ##   ## #####",
+    "### ###### ####### ##   ###",
+    "###     ##     ###   ##  ##",
+    "### ### ######   # #  #  ##",
+    "##   ##      ##    #    ###",
+    "############### ############"
+]
+
+root = tk.Tk()
+root.title("Labirinto e Caminho mais Curto")
+
+# Exibindo o título centralizado na cor roxa
+title_label = tk.Label(root, text="Caminhando contra o BUG", font=("Arial", 16), fg="purple")
+title_label.pack()
+
+# Exibindo o labirinto inicial na tela
+labirinto_txt = "\n".join(labirinto)
+labirinto_label = tk.Label(root, text=labirinto_txt, font=("Courier", 12))
+labirinto_label.pack()
+
+btn_ver_caminho = tk.Button(root, text="Ver Caminho", command=on_ver_caminho)
+btn_ver_caminho.pack()
+
+root.mainloop()
